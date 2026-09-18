bfe6f5cc9afd8a6e06cb0973a7be45bde88c85bd
Swarnali2101 <swarnalimitra2004@gmail.com>
2026-09-18 10:51:02 +0530

    Python set practice problems

--- Data Structures/Topics To Learn/Tuple/lastElement.py
+++ Data Structures/Topics To Learn/Tuple/lastElement.py
@@ -1,6 +1,12 @@
 numbers = [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
 result = []
-for t in numbers:
-    new_tuple = t[:-1] + (100,)
-    result.append(new_tuple)
-print(result)
+#for t in numbers:
+    #new_tuple = t[:-1] + (100,)
+    #result.append(new_tuple)
+#print(result)
+
+for index, tup in enumerate(numbers):
+    temp = list(tup)
+    temp[-1] = 100
+    numbers[index] = tuple(temp)
+print(numbers)
