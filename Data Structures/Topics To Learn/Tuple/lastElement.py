a535c66b07259f33121f980fcbae25edcae60f55
Swarnali2101 <swarnalimitra2004@gmail.com>
2026-09-18 11:02:14 +0530

    Python tuple practice problems

--- Data Structures/Topics To Learn/Tuple/lastElement.py
+++ Data Structures/Topics To Learn/Tuple/lastElement.py
@@ -2,7 +2,7 @@ numbers = [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
 result = []
 #for t in numbers:
     #new_tuple = t[:-1] + (100,)
-    #result.append(new_tuple)
+    #result.append(new_tuple)#
 #print(result)
 
 for index, tup in enumerate(numbers):
